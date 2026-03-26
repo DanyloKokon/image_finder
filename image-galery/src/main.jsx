07b8b48dc54b7ce8a8757ee36f71b3732f06bf67
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2026-03-26 19:49:52 +0100

    sr

--- image-galery/src/main.jsx
+++ image-galery/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+ import './index.css'
 import App from './Apps/App.jsx'
-import MainApp from './Apps/MainApp.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MainApp />
+    <App />
   </StrictMode>,
 )
